4c40d0933b7c6910b18eeb2927c099cdc7cfc862
Mandy B <mandyblott@gmail.com>
2021-06-17 15:18:37 -0500

    Corrected error in seed file

--- prep/book-wishlist-seed.sql
+++ prep/book-wishlist-seed.sql
@@ -10,16 +10,16 @@ VALUES ('First booklist', 'Fun books to read when relaxing', 'testuser'),
        ('Second booklist', 'Books I heard good things about', 'testuser');
 
 INSERT INTO books (isbn, title, author, bestsellers_date, type)
-VALUES ('9780316492935', 'HOW THE WORD IS PASSED', 'Clint Smith', '2021-06-05', 'nonfiction'),
-       ('9781728251059', 'FREED', 'E.L. James', '2021-06-05', 'fiction'),
-       ('9781538748169', 'THE BOY FROM THE WOODS', 'Harlan Coben', '2020-03-21', 'fiction'),
-       ('9780385348713', 'THE SPLENDID AND THE VILE', 'Erik Larson', '2020-03-21', 'nonfiction');
+VALUES ('9780399589010', 'ZERO FAIL', 'Carol Leonnig', '2021-06-06', 'nonfiction'),
+       ('9781501171369', 'THE LAST THING HE TOLD ME', 'Laura Dave', '2021-06-06', 'fiction'),
+       ('9781250223180', 'WHAT HAPPENED TO YOU?', 'Bruce D. Perry and Oprah Winfrey', '2021-06-06', 'nonfiction'),
+       ('9780385547680', 'SOOLEY', 'John Grisham', '2021-06-06', 'fiction');
 
 INSERT INTO books_on_lists (isbn, booklist_id)
-VALUES ('9780316492935', 1),
-       ('9781538748169', 1),
-       ('9781728251059', 2),
-       ('9780385348713', 2);
+VALUES ('9780399589010', 1),
+       ('9781501171369', 1),
+       ('9781250223180', 2),
+       ('9780385547680', 2);
 
        
        
